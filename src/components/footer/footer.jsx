66cb5d1d989965248ab5e2e01f4a88c7931394a8
Louis Ferraro <louisferraro@louiss-mbp-2.home>
2020-12-03 11:44:47 -0500

    adding everything

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer text-center">
+      Event Dashboard for COP4710 - University of Central Florida
+    </footer>
+  );
+};
+export default Footer;
